7f7e1cc8f38c319f62454f1d77f66c5cb598c007
error404-sp <shreyasipatil404@gmail.com>
2021-07-24 14:33:13 +0530

    space complexity

--- CTCI/Big O/index.js
+++ CTCI/Big O/index.js
@@ -12,3 +12,22 @@ function addUpTo(){
 }
 //this is O(1)
 
+//// SPACE COMPLEXITY
+
+function sum(arr){
+    let sum = 0;
+    for(let i=0;i<arr.length;i++){
+        total+=arr[i];
+    }
+    return total;
+}
+
+    //this is o(1)
+
+    function double(arr){
+        let newArr = [];
+        for(let i=0;i<arr.length;i++){
+            newArr.push(2*arr[i]);
+        }
+        return newArr;
+    }
